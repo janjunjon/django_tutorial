25a20e80f6e2b60229adfc814073849184bd68d5
janjunjon <ishihara@lograph.co.jp>
2022-04-21 17:08:05 +0900

    edit reffering auth vars again

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zs$o+m-arulorjgv85x&($**x36=9a8*rl-a6$1ug-bt5xeems'
+SECRET_KEY = os.environ["django_secret_key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
